668ffb8c217bcf51257e786409c030c6d758f361
Ryan McAdler <ryanmcadler@gmail.com>
2017-05-12 00:15:09 -0700

    clean up

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,7 @@ var app = angular.module('LookupTZ', []);
 
 app.controller('formController', function($scope, $http) {
 
+  // init...
   // initialize map...
   var map = L.map('map').fitWorld();
   var osmMapnikBasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,29 +11,32 @@ app.controller('formController', function($scope, $http) {
   }).addTo(map);
   map.locate({setView: true, maxZoom: 16});
 
+  // map zipcode...
+  $scope.mapZipcode = function(map, tzName, zipcode) {
+    if (typeof(zipcode.lat) !== 'undefined' && typeof(zipcode.lng) !== 'undefined') {
+      map.setView([zipcode.lat, zipcode.lng], 14);
+      // invoke popup...
+      var popupContent = '<b>Zipcode:</b> ' + zipcode.zipcode + '<br />';
+      popupContent += '<b>ActiveSupport TimeZone</b>:<br />' + tzName + ' (UTC' + zipcode.utc_offset + ')<br />';
+      var marker = L.marker([zipcode.lat, zipcode.lng]).addTo(map).bindPopup(popupContent).openPopup();
+    }
+  };
+
   // form submit...
   $scope.submitForm = function() {
     console.log($scope)
+
+    $('.help-block').empty();
+
     var data = {
       zipcode: $scope.zipcode
     };
 
     $http.post('/zipcodes/find', JSON.stringify(data)).then(function(resp){
-
       var tzName = resp.data.tz_name;
       var zipcode = resp.data.zipcode;
-
       console.log(resp);
-
-      // show results on map...
-      if (typeof(zipcode.lat) !== 'undefined' && typeof(zipcode.lng) !== 'undefined') {
-        map.setView([zipcode.lat, zipcode.lng], 14);
-        // invoke popup...
-        var popupContent = '<b>Zipcode:</b> ' + zipcode.zipcode + '<br />';
-        popupContent += '<b>ActiveSupport TimeZone</b>:<br />' + tzName + ' (UTC' + zipcode.utc_offset + ')<br />';
-        var marker = L.marker([zipcode.lat, zipcode.lng]).addTo(map).bindPopup(popupContent).openPopup();
-      }
-
+      $scope.mapZipcode(map, tzName, zipcode);
     }, function(resp){
       $('.help-block').html(resp.data.errors);
       $('.form-group').addClass('has-error');
