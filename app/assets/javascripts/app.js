cbe7b180888645edf23fd63e5013e32289e3919d
Ryan McAdler <ryanmcadler@gmail.com>
2017-05-11 22:00:08 -0700

    scaffold out basic angular app

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,14 @@
-var app = angular.module("LookupTZ", []);
+var app = angular.module('LookupTZ', []);
+
+app.controller('formController', function($scope, $http) {
+
+  $scope.data = {
+    zipcode: "default"
+  };
+
+  $scope.submitForm = function() {
+    console.log("posting data....", $scope.zipcode);
+    //$http.post('http://...', JSON.stringify(data)).success(function(){/*success callback*/});
+  };
+
+});
