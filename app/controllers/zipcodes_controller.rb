d73a3d7fe6cd5d4e3db3322719b83a1073b0020d
Ryan McAdler <ryanmcadler@gmail.com>
2017-05-14 18:07:44 -0700

    return 400 from zipcodes#find when request is sent with no zipcodes parameter

--- app/controllers/zipcodes_controller.rb
+++ app/controllers/zipcodes_controller.rb
@@ -1,16 +1,22 @@
 class ZipcodesController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def find
-    zip = Zipcode.find_by_zipcode(params[:zipcode])
-    if zip.present?
-      tz = ActiveSupport::TimeZone[zip.utc_offset]
-      render status: 200, json: {
-        tz_name: tz.tzinfo.name,
-        zipcode: zip.slice('city', 'dst', 'lat', 'lng', 'state', 'utc_offset', 'zipcode')
-      }
+    if params[:zipcode].present?
+      zip = Zipcode.find_by_zipcode(params[:zipcode])
+      if zip.present?
+        tz = ActiveSupport::TimeZone[zip.utc_offset]
+        render status: 200, json: {
+          tz_name: tz.tzinfo.name,
+          zipcode: zip.slice('city', 'dst', 'lat', 'lng', 'state', 'utc_offset', 'zipcode')
+        }
+      else
+        render status: 404, json: {
+          errors: 'Zipcode not found'
+        }
+      end
     else
-      render status: 404, json: {
-        errors: 'Zipcode not found'
+      render status: 400, json: {
+        errors: 'Bad request; no zipcode parameter'
       }
     end
   end
