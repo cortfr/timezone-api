c06e00f626d2d7148567e16ba337069d7f92de66
Ryan McAdler <ryanmcadler@gmail.com>
2017-05-14 18:27:28 -0700

    better tests for pages controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  
   test "should get index" do
     get pages_index_url
     assert_response :success
+    assert_select 'div#map'
+    assert_select 'div#testConsole'
   end
 
 end
