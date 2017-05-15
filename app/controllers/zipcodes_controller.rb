2815679d3177a5d07eba667ffc3ced48a2f29642
Ryan McAdler <ryanmcadler@gmail.com>
2017-05-14 19:03:05 -0700

    write tests for controllers

--- app/controllers/zipcodes_controller.rb
+++ app/controllers/zipcodes_controller.rb
@@ -1,6 +1,6 @@
 class ZipcodesController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter :ensure_json_request
+  #before_filter :ensure_json_request
 
   def ensure_json_request
     return if request.format == :json
@@ -15,7 +15,7 @@ class ZipcodesController < ApplicationController
       if zip.present?
         tz = ActiveSupport::TimeZone[zip.utc_offset]
         render status: 200, json: {
-          tz_name: tz.tzinfo.name,
+          tz_name: tz.name,
           zipcode: zip.slice('city', 'dst', 'lat', 'lng', 'state', 'utc_offset', 'zipcode')
         }
       else
--- test/controllers/zipcodes_controller_test.rb
+++ test/controllers/zipcodes_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class ZipcodesControllerTest < ActionDispatch::IntegrationTest
-  test "should get find" do
-    get zipcodes_find_url
+
+  test "should return 200, zipcode" do
+    post zipcodes_find_url, params: { zipcode: 97701 }
     assert_response :success
+  end
+
+  test "should return 404" do
+    post zipcodes_find_url, params: { zipcode: 99999 }
+    assert_equal 404, status
+  end
+
+  test "should return 400" do
+    post zipcodes_find_url, params: { derp: true }
+    assert_equal 400, status
   end
 
 end
