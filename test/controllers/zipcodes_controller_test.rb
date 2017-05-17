8d28d9e823553010cc97f2b73f4cb61aea8f7606
Ryan McAdler <ryanmcadler@gmail.com>
2017-05-16 19:08:44 -0700

    improve quality of tests

--- test/controllers/zipcodes_controller_test.rb
+++ test/controllers/zipcodes_controller_test.rb
@@ -17,4 +17,10 @@ class ZipcodesControllerTest < ActionDispatch::IntegrationTest
     assert_equal 400, status
   end
 
+  test "should return Pacific" do
+    post zipcodes_find_url, params: { zipcode: 97701 }
+    json = JSON.parse(response.body)
+    assert_equal 'Pacific Time (US & Canada)', json['tz_name']
+  end
+
 end
